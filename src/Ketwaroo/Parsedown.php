ece7b97ffee9f3b3c848bd53bf61d1365e472849
Ketwaroo Yaasir <ketwaroo.yaasir@gmail.com>
2021-09-10 10:29:08 -0400

    Update Parsedown.php

--- src/Ketwaroo/Parsedown.php
+++ src/Ketwaroo/Parsedown.php
@@ -1,17 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Ketwaroo;
 
 /**
  * Customisations to erusev/parsedown-extra
  *
- * @author Yaasir Ketwaroo<ketwaroo.yaasir@gmail.com>
+ * @author Yaasir Ketwaroo
  */
 class Parsedown extends \ParsedownExtra
 {
